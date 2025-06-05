fdacd47a79910cca381ed32321d0d5bcf83b60b6
SujeshShakya4130 <sujeshshakya4130@gmail.com>
2025-06-05 21:48:29 +0545

    Add files via upload

---
+++ MAY30_birthdate_calculator.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+birth_input = input("Enter your birth date (YYYY-MM-DD): ")
+
+# Your birthdate
+birth_date = datetime.strptime(birth_input, "%Y-%m-%d")
+
+# Todays date
+today = datetime.today()
+
+# Calculate the difference in days
+days_old = (today - birth_date).days
+
+# Calculate the difference in years
+years_old = days_old // 365
+
+print(f"You are {days_old} days old.")
+print(f"You are {years_old} years old.")
